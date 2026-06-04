4f149a26a0cf3f222fde28e25ea458759e8c1d37
Anthony Ogburn <ajogburn96@protonmail.com>
2026-06-04 13:00:27 -0500

    feat: expand product catalog to 20 popular real items from dc32168168 Yupoo albums
    - Replaced Unsplash placeholders with actual supplier photos (medium.jpg)
    - Curated top hype models based on visibility/popularity signals: multiple Jordan collabs (Travis, Nigel Sylvester, Awake NY, Mother's Day 11s, True Blue 3s, Comic/Birds 4s, Bloodline 12s, BIN23/Oreo 6s), Kobe 5, Balenciaga Jet, OFF-WHITE, NB 9060, Air Max 95 specials, AJ17, AJ10
    - Prices adjusted for collabs vs core models (135-220 range)
    - Shop, product detail, cart, featured, and checkout now reflect the expanded authentic catalog
    - All images use onerror fallback already present in templates

--- product.js
+++ product.js
@@ -2,51 +2,123 @@
 const products = [
   {
     id: 1,
-    name: "Travis Scott x Air Jordan 1 Low OG",
-    price: 165,
-    img: "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2"
+    name: "Travis Scott x Air Jordan 1 Low OG TS SP",
+    price: 220,
+    img: "https://photo.yupoo.com/dc32168168/6a928804d2/medium.jpg"
   },
   {
     id: 2,
-    name: "Nigel Sylvester x Jordan 4 OG Bike",
-    price: 220,
-    img: "https://images.unsplash.com/photo-1600185365483-26d7a16503c6"
+    name: "Nigel Sylvester x Jordan 4 OG",
+    price: 210,
+    img: "https://photo.yupoo.com/dc32168168/1773d0435a/medium.jpg"
   },
   {
     id: 3,
-    name: "Air Jordan 11 GS Rare Air",
-    price: 180,
-    img: "https://images.unsplash.com/photo-1605348532760-6753d2c43329"
+    name: "Awake NY x Air Jordan 6 'Playful Pink'",
+    price: 195,
+    img: "https://photo.yupoo.com/dc32168168/576bdeb251/medium.jpg"
   },
   {
     id: 4,
-    name: "Nike SB Dunk Low Nardwuar",
-    price: 135,
-    img: "https://images.unsplash.com/photo-1549298916-b41d501d3772"
+    name: "Jordan 11 Retro 'Mother's Day'",
+    price: 180,
+    img: "https://photo.yupoo.com/dc32168168/009ab8954b/medium.jpg"
   },
   {
     id: 5,
-    name: "Puma Speedcat Red White",
-    price: 95,
-    img: "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa"
+    name: "Jordan 3 Retro 'White and True Blue' (GX)",
+    price: 165,
+    img: "https://photo.yupoo.com/dc32168168/5c68679c7b/medium.jpg"
   },
   {
     id: 6,
-    name: "SoleFly x Air Jordan 8",
-    price: 195,
-    img: "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2"
+    name: "Jordan 3 Retro OG 'Brasil'",
+    price: 155,
+    img: "https://photo.yupoo.com/dc32168168/278a4d4fdd/medium.jpg"
   },
   {
     id: 7,
-    name: "Nike Pegasus Premium Forrest Gump",
-    price: 145,
-    img: "https://images.unsplash.com/photo-1600585154340-be6161a56a9c"
+    name: "Jordan 4 Retro 'Comic'",
+    price: 165,
+    img: "https://photo.yupoo.com/dc32168168/cdea8fa0be/medium.jpg"
   },
   {
     id: 8,
-    name: "Air Jordan 1 Low Green Red",
-    price: 125,
-    img: "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2"
+    name: "Jordan 12 Retro 'Bloodline' (GX)",
+    price: 170,
+    img: "https://photo.yupoo.com/dc32168168/ef75d55afa/medium.jpg"
+  },
+  {
+    id: 9,
+    name: "Jordan 6 Retro 'Oreo'",
+    price: 155,
+    img: "https://photo.yupoo.com/dc32168168/30bb926281/medium.jpg"
+  },
+  {
+    id: 10,
+    name: "Jordan 6 'BIN23'",
+    price: 160,
+    img: "https://photo.yupoo.com/dc32168168/cb9bc7e2/medium.jpg"
+  },
+  {
+    id: 11,
+    name: "Nike Zoom Kobe 5 Protro 'Dodgers'",
+    price: 145,
+    img: "https://photo.yupoo.com/dc32168168/7f545fae73/medium.jpg"
+  },
+  {
+    id: 12,
+    name: "Balenciaga Jet Low 'Shell Yellow'",
+    price: 195,
+    img: "https://photo.yupoo.com/dc32168168/830b5150f8/medium.jpg"
+  },
+  {
+    id: 13,
+    name: "OFF-WHITE Low Top Brown",
+    price: 175,
+    img: "https://photo.yupoo.com/dc32168168/616d53d6e9/medium.jpg"
+  },
+  {
+    id: 14,
+    name: "New Balance 9060 Grey",
+    price: 135,
+    img: "https://photo.yupoo.com/dc32168168/b6a361928a/medium.jpg"
+  },
+  {
+    id: 15,
+    name: "Nike Air Max 95 'Grape' Big Bubble OG",
+    price: 150,
+    img: "https://photo.yupoo.com/dc32168168/9c3fcbeef1/medium.jpg"
+  },
+  {
+    id: 16,
+    name: "UNDEFEATED x Nike Air Max 95 SP",
+    price: 155,
+    img: "https://photo.yupoo.com/dc32168168/5a6d2d5cdb/medium.jpg"
+  },
+  {
+    id: 17,
+    name: "Air Jordan 17 Low SP 'Lightning'",
+    price: 145,
+    img: "https://photo.yupoo.com/dc32168168/216e6fb4d0/medium.jpg"
+  },
+  {
+    id: 18,
+    name: "Jordan 10 Retro 'Red Steel'",
+    price: 145,
+    img: "https://photo.yupoo.com/dc32168168/b363520458/medium.jpg"
+  },
+  {
+    id: 19,
+    name: "Jordan 4 'Birds of Paradise'",
+    price: 160,
+    img: "https://photo.yupoo.com/dc32168168/a65840902c/medium.jpg"
+  },
+  {
+    id: 20,
+    name: "Jordan 3 Retro 'World's Best Dad'",
+    price: 160,
+    img: "https://photo.yupoo.com/dc32168168/1991365bf2/medium.jpg"
   }
 ];
 
